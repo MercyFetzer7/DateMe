f4a2e100887d4ea18783159e5c682cdc0dadd4b8
Mercy Fetzer <131705906+MercyFetzer7@users.noreply.github.com>
2025-02-09 13:57:32 -0700

    1:57 update

--- DateMe/Controllers/HomeController.cs
+++ DateMe/Controllers/HomeController.cs
@@ -6,26 +6,20 @@ namespace DateMe.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
     }
-
-    public IActionResult Privacy()
+    
+    [HttpGet]
+    public IActionResult DatingApplication()
     {
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpPost]
+    public IActionResult DatingApplication(Application response)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View("Confirmation", response);
     }
 }
--- DateMe/Models/Application.cs
+++ DateMe/Models/Application.cs
@@ -2,5 +2,13 @@ namespace DateMe.Models;
 
 public class Application
 {
-    
+    // public int ApplicationID { get;} // read only variable
+    public int ApplicationID { get; set;}
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Major { get; set; }
+    public string Occupation { get; set; }
+    public bool CreeperStalker { get; set; }
 }
